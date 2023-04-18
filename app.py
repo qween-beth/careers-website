20ee5f2e929f616853e4b84abb3f33f291cb15e1
qween-beth <103456558+qween-beth@users.noreply.github.com>
2023-04-18 09:06:04 +0000

    added data stream

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location': 'Lagos, Nigeria',
+    'salary': 'N100,000'
+  },
+  {
+      'id':2,
+      'title': 'Data Scientist',
+      'location': 'Ondo, Nigeria',
+      'salary': 'N100,000'
+    },
+  {
+    'id':3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': 'N100,000'
+  },
+  {
+    'id':4,
+    'title': 'Data Analyst',
+    'location': 'Delta, Nigeria',
+    'salary': 'N100,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template('home.html', jobs=JOBS, company_name='CareerWoman')
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', debug=True)
